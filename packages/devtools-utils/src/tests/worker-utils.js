495b7188811c7d8c5ad145f9f3c3a513dadea27e
Jason Laster <jason.laster.11@gmail.com>
2017-07-05 19:48:52 -0400

    streaming worker

--- packages/devtools-utils/src/tests/worker-utils.js
+++ packages/devtools-utils/src/tests/worker-utils.js
@@ -2,15 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { WorkerDispatcher } = require("../worker-utils")
+const { WorkerDispatcher, streamingWorkerHandler } = require("../worker-utils");
 
 describe("worker utils", () => {
   it("starts a worker", () => {
     const dispatcher = new WorkerDispatcher();
-    global.Worker = jest.fn()
+    global.Worker = jest.fn();
     dispatcher.start("foo");
-    expect(dispatcher.worker).toEqual(global.Worker.mock.instances[0])
-  })
+    expect(dispatcher.worker).toEqual(global.Worker.mock.instances[0]);
+  });
 
   it("stops a worker", () => {
     const dispatcher = new WorkerDispatcher();
@@ -19,14 +19,14 @@ describe("worker utils", () => {
     global.Worker = jest.fn(() => {
       return {
         terminate: terminateMock
-      }
+      };
     });
 
     dispatcher.start();
     dispatcher.stop();
 
-    expect(dispatcher.worker).toEqual(null)
-    expect(terminateMock.mock.calls.length).toEqual(1)
+    expect(dispatcher.worker).toEqual(null);
+    expect(terminateMock.mock.calls.length).toEqual(1);
   });
 
   it("dispatches a task", () => {
@@ -38,24 +38,58 @@ describe("worker utils", () => {
       return {
         postMessage: postMessageMock,
         addEventListener: addEventListenerMock
-      }
+      };
     });
 
     dispatcher.start();
-    const task = dispatcher.task("foo")
-    task("bar")
+    const task = dispatcher.task("foo");
+    task("bar");
 
     const postMessageMockCall = postMessageMock.mock.calls[0][0];
 
     expect(postMessageMockCall).toEqual({
-        "args": [
-          "bar",
-        ],
-        "id": 1,
-        "method": "foo",
+      args: ["bar"],
+      id: 1,
+      method: "foo"
     });
 
     expect(addEventListenerMock.mock.calls.length).toEqual(1);
-  })
+  });
 
-})
+  fit("streams a task", async () => {
+    const postMessageMock = jest.fn();
+    jest.useFakeTimers();
+
+    let mocks = [];
+    for (let i = 0; i < 10; i++) {
+      const fn = jest.fn();
+      mocks.push(fn);
+      // fn();
+    }
+
+    function makeTasks() {
+      console.log("YO");
+      return [
+        {
+          callback: () => new Promise(resolve => setTimeout(() => resolve(2), 50))
+        },
+        {
+          callback: () => new Promise(resolve => setTimeout(() => resolve(1), 50))
+        }
+      ];
+    }
+
+    const worker = {
+      postMessage: postMessageMock
+    };
+
+    const workerHandler = streamingWorkerHandler(worker, { makeTasks });
+    const task = workerHandler({ data: { id: 2, method: "makeTasks", args: [] } });
+
+    // jest.runTimersToTime(320);;
+    await task;
+
+    console.log(postMessageMock.mock.calls.map(cal => JSON.stringify(cal)));
+    console.log(mocks.map(mock => mock.mock.calls));
+  });
+});
--- packages/devtools-utils/src/worker-utils.js
+++ packages/devtools-utils/src/worker-utils.js
@@ -57,22 +57,71 @@ WorkerDispatcher.prototype = {
       });
     };
   }
-}
+};
 
 function workerHandler(publicInterface: Object) {
   return function workerHandler(msg: Message) {
     const { id, method, args } = msg.data;
     const response = publicInterface[method].apply(undefined, args);
     if (response instanceof Promise) {
-      response.then(val => self.postMessage({ id, response: val }),
-                    err => self.postMessage({ id, error: err }));
+      response.then(
+        val => self.postMessage({ id, response: val }),
+        err => self.postMessage({ id, error: err })
+      );
     } else {
       self.postMessage({ id, response });
     }
   };
 }
 
+function streamingWorkerHandler(ctx, publicInterface: Object, { timeout = 100 } = {}) {
+  async function lazyWorker(ctx, tasks, id, timeout) {
+    let isWorking = true;
+    console.log("lazyWorker", tasks.length);
+
+    setTimeout(() => {
+      console.log("STOP!");
+      isWorking = false;
+    }, timeout);
+
+    const results = [];
+    while (tasks.length != 0 && isWorking) {
+      const { callback, context, args } = tasks.pop();
+      console.log("tasks", tasks.length, callback);
+      const result = await callback.apply(context, args);
+      console.log("result", result, callback);
+      results.push(result);
+    }
+
+    ctx.postMessage({ id, results });
+    if (tasks.length != 0) {
+      await lazyWorker(ctx, tasks, id, timeout);
+    }
+  }
+
+  return async function (msg: Message) {
+    const { id, method, args } = msg.data;
+    const workerMethod = publicInterface[method];
+    if (!workerMethod) {
+      console.error(`Could not find ${method} defined in worker.`);
+    }
+
+    ctx.postMessage({ id, start: true });
+
+    try {
+      const tasks = workerMethod.apply(null, args);
+      await lazyWorker(ctx, tasks, timeout);
+      ctx.postMessage({ id, done: true });
+    } catch (error) {
+      ctx.postMessage({ id, error });
+    }
+
+    console.log("YO 6");
+  };
+}
+
 module.exports = {
   WorkerDispatcher,
-  workerHandler
+  workerHandler,
+  streamingWorkerHandler
 };
